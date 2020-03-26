b90039a8bf9861ad6ba3f6e169d2f5b93710ee65
Deep <srideepmaulik@gmail.com>
2020-03-26 13:40:42 +0000

    Changes in main.py and node/relationship definitions in graphdb.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
+counties_df = pd.read_csv('NJ County Profiles.csv', header=0)
 teachers_df = pd.read_csv('NJ Teachers Salaries 2016.csv', header=0)
-print(teachers_df.columns)
+towns_df = pd.read_csv('NJ Towns Profiles.csv',header=0)
+
+print(" ")
+print('Loading NJ Counties data')
+print(" ")
+print(counties_df.head(3))
+print(" ")
+print('Loading NJ Teachers data')
+print(" ")
+print(teachers_df.head(3))
+print(" ")
+print('Loading NJ Towns data')
+print(" ")
+print(towns_df.head(3))
